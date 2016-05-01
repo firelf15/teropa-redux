b4e0d92365b67ac9cf447f8989bf2b1eee214932
FirElf15 <github@firelf.com>
2016-04-30 20:51:59 -0700

    better immutable tree passing

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -42,38 +42,33 @@ describe('immutability', () => {
 
     });
 
-    /*    describe('a tree', () => {
+    describe('a tree', () => {
 
-            function addMovie(currentState, movie) {
-                return currentState.set(
-                    'movies',
-                    currentState.get('movies').push(movie)
-                );
-            }*/
+        function addMovie(currentState, movie) {
+            return currentState.update('movies', movies => movies.push(movie));
+        }
 
-    function addMovie(currentState, movie) {
-        return currentState.update('movies', movies => movies.push(movie));
-    }
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
+            let nextState = addMovie(state, 'Sunshine');
 
-    it('is immutable', () => {
-        let state = Map({
-            movies: List.of('Trainspotting', '28 Days Later')
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later',
+                    'Sunshine'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later'
+                )
+            }));
         });
-        let nextState = addMovie(state, 'Sunshine');
 
-        expect(nextState).to.equal(Map({
-            movies: List.of(
-                'Trainspotting',
-                '28 Days Later',
-                'Sunshine'
-            )
-        }));
-        expect(state).to.equal(Map({
-            movies: List.of(
-                'Trainspotting',
-                '28 Days Later'
-            )
-        }));
     });
 
 });
